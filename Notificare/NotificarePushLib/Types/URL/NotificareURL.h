d24ab3f6a7bd394dc5e2346334221e3ab4329941
jjonthemix <joeloliveira1979@gmail.com>
2013-03-01 23:45:02 +0100

    added new type URL

---
+++ Notificare/NotificarePushLib/Types/URL/NotificareURL.h
@@ -0,0 +1,32 @@
+//
+//  NotificareURL.h
+//  NotificarePushLib
+//
+//  Created by Joel Oliveira on 3/1/13.
+//  Copyright (c) 2013 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Notification.h"
+#import "Action.h"
+#import "Content.h"
+#import "Attachment.h"
+#import "NotificationType.h"
+#import "NotificationDelegate.h"
+#import "NotificareActions.h"
+
+@interface NotificareURL : NSObject <NotificationType,UIWebViewDelegate,UIActionSheetDelegate>
+
+@property (nonatomic, assign) id<NotificationDelegate> delegate;
+@property (nonatomic, strong) Notification * notification;
+@property (nonatomic, strong) NotificareActions * notificareActions;
+@property (strong, nonatomic) UIViewController * rootViewController;
+@property (strong, nonatomic) UIBarButtonItem * closeButton;
+@property (strong, nonatomic) UIBarButtonItem * actionsButton;
+@property (strong, nonatomic) UIActionSheet *actionSheet;
+
+
+
+-(void)openNotification;
+
+@end

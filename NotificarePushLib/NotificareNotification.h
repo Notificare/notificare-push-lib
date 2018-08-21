e3ee70ebba42fca94385caee9eda56518627a0f6
Joel Oliveira <joel@notifica.re>
2018-08-21 12:54:10 +0200

    - Fixed issue with NotificareNotification object not correctly handling title and subtitle

--- NotificarePushLib/NotificareNotification.h
+++ NotificarePushLib/NotificareNotification.h
@@ -22,9 +22,9 @@
 @property (strong, nonatomic, nullable) NSString * notificationTitle;
 @property (strong, nonatomic, nullable) NSString * notificationSubtitle;
 @property (strong, nonatomic, nonnull) NSString * notificationMessage;
-@property (strong, nonatomic, nullable) NSArray * notificationContent;
-@property (strong, nonatomic, nullable) NSArray * notificationActions;
-@property (strong, nonatomic, nullable) NSArray * notificationAttachments;
+@property (strong, nonatomic, nullable) NSArray<NotificareContent*> * notificationContent;
+@property (strong, nonatomic, nullable) NSArray<NotificareAction*> * notificationActions;
+@property (strong, nonatomic, nullable) NSArray<NotificareAttachment*> * notificationAttachments;
 @property (strong, nonatomic, nullable) NSDictionary * notificationExtra;
 @property (strong, nonatomic, nullable) NSDictionary * notificationInfo;
 
